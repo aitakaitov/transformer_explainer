7bb5acf2cc2bf377b15a738ff057d7caa921d35b
aitakaitov <35534313+aitakaitov@users.noreply.github.com>
2021-11-25 18:54:00 +0100

    Delete update_meta.bat

--- update_meta.bat
+++
@@ -1,9 +0,0 @@
-ssh barticka@minos.zcu.cz
-cd transformer/explanations
-rm meta-*
-rm -rf utils
-rm -rf explainers
-exit
-scp -r "C:\Users\Vojtěch Bartička\PycharmProjects\transformer_explainer\meta-*" barticka@minos.zcu.cz:~/transformer/explanations
-scp -r "C:\Users\Vojtěch Bartička\PycharmProjects\transformer_explainer\utils" barticka@minos.zcu.cz:~/transformer/explanations
-scp -r "C:\Users\Vojtěch Bartička\PycharmProjects\transformer_explainer\explainers" barticka@minos.zcu.cz:~/transformer/explanations
